6e35300e69d01aa1a2cdd0a3207cef520cc4d2c2
Salman Ahmad <s.ahmad0147@gmail.com>
2025-11-22 15:10:47 -0700

    Fix navbar overlap issue and update last modified date to Nov 22

--- src/constants/lastUpdated.ts
+++ src/constants/lastUpdated.ts
@@ -1,2 +1,2 @@
 // This file is auto-updated at build time to reflect the latest commit date for static hosting.
-export const LAST_UPDATED = 'Nov 4, 2025';
+export const LAST_UPDATED = 'Nov 22, 2025';
